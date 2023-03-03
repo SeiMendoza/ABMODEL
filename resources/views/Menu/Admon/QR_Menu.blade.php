6bb5142e9146536a690144d3c62b36ecbe2b5450
Ejlopez10 <93482799+Ejlopez10@users.noreply.github.com>
2023-03-03 11:16:57 -0600

    Update QR_Menu.blade.php

--- resources/views/Menu/Admon/QR_Menu.blade.php
+++ resources/views/Menu/Admon/QR_Menu.blade.php
@@ -1,5 +1,5 @@
 @extends('00_plantillas_Blade.plantilla_General')
-@section('contend')
+@section('activatedMenu')
 
 <br>
  
@@ -8,8 +8,8 @@
    
      {!!   QrCode::Color(255, 0, 0)->size(200)->errorCorrection('H')
         ->encoding('UTF-8')->style('square')->format('svg')
-        ->margin(10)->generate('http://127.0.0.1:8000/menu/cliente'); !!}
-        <h4 style="text-align: center; color:azure">Mesa 1</h4>
+        ->margin(10)->generate('http://127.0.0.1:8000/menu/prueba'); !!}
+        <h4 style="text-align: center; color:teal">Mesa 1</h4>
         
         </div>
     
