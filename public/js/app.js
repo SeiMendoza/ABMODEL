ffd466baf21b5ecfdb3a75e3928591dd0a1ac2dd
rolnd30 <114603660+rolnd30@users.noreply.github.com>
2023-03-20 23:17:21 -0600

    Revert "correccion funcion para precargar imagen"
    This reverts commit 1dd457c154188ae2e72188e70936089b651c2a2d.

--- public/js/app.js
+++ public/js/app.js
@@ -10,24 +10,6 @@ function rellenar2(){
     document.getElementById("precio3").value = document.getElementById("precio").value;
 }
 
-function precargar(a){
-    const $seleccionArchivos = document.querySelector("#imagen"+a);
-    const $imagenPrevisualizacion = document.querySelector("#imagenmostrada"+a);
-
-    const archivos = $seleccionArchivos.files;
-    // Si no hay archivos salimos de la función y quitamos la imagen
-    if (!archivos || !archivos.length) {
-        $imagenPrevisualizacion.src = "";
-        return;
-    }
-    // Ahora tomamos el primer archivo, el cual vamos a previsualizar
-    const primerArchivo = archivos[0];
-    // Lo convertimos a un objeto de tipo objectURL
-    const objectURL = URL.createObjectURL(primerArchivo);
-    // Y a la fuente de la imagen le ponemos el objectURL
-    $imagenPrevisualizacion.src = objectURL;
-}
-
 const $seleccionArchivos = document.querySelector("#imagen"),
 $imagenPrevisualizacion = document.querySelector("#imagenmostrada");
 
