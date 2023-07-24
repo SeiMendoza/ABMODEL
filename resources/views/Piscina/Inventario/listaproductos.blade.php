0d8d71d8a1abd3790bf9c38e9e2b4cfc4d6401f3
rolnd30 <rolandohernndez@unah.hn>
2023-07-24 17:29:26 -0600

    Correccion de tabla

--- resources/views/Piscina/Inventario/listaproductos.blade.php
+++ resources/views/Piscina/Inventario/listaproductos.blade.php
@@ -58,6 +58,7 @@ style="position: absolute; top:100%;left:1%">Productos de piscina</h3>--->
             <tr>
                 <th scope="col" style="text-align:center">N</th>
                 <th scope="col" style="text-align:center; display: nome">id</th>
+                <th scope="col" style="text-align:center; display: nome">cantidad</th>
                 <th scope="col" style="text-align:center">Producto</th>
                 <th scope="col" style="text-align:center;text-transform:initial;">Tipo de producto</th>
                 <th scope="col" style="text-align:center">Cantidad</th>
@@ -67,52 +68,56 @@ style="position: absolute; top:100%;left:1%">Productos de piscina</h3>--->
         </thead>
         <tbody>
             @forelse($prod as $i => $p)
-            <tr style="text-align:center;">
-                <td scope="col">{{++$i}}</td>
-                <td scope="col" style="text-align:center; display: nome">{{$p->id}}</td>
-                <td scope="col">{{$p->nombre}}</td>
-                <td scope="col">{{$p->tipo_producto->descripcion}}</td>
-                <td scope="col">
-                    <!--Boton para restar cantidad-->
-                    <a style="color: red;" class="productpiscina" data-toggle="modal" data-target="#restar{{$p->id}}">
-                        <i class="fa fa-minus-circle" aria-hidden="true"></i>
-                    </a>
-                    <!--Texto de cantidad-->
-                    {{$p->peso}} @if ($p->tipo_producto->id == 1) Libras @else Onzas @endif
-                    <!--Boton para agregar cantidad-->
-                    <a style="color: blue;" class="productpiscina" data-toggle="modal" data-target="#agregar{{$p->id}}">
-                        <i style="text-size-adjust: 2px;" class="fa fa-plus-circle" aria-hidden="true"></i>
-                    </a>
-                </td>
-                <td>
-                    <a href="{{ route('producto.edit', ['id' => $p->id]) }}">
-                        <i class="fa-solid fa-edit text-success" style="color:rgb(33, 195, 247)"></i>
-                </td>
-                <!----icono para borrar el producto------>
-                <td>
-                    <i data-bs-toggle="modal" data-bs-target="#staticBackdropE{{$p->id}}" class="fa-solid fa-trash-can text-danger" style="color:crimson"></i>
-                    <form action="{{route('prodpiscina.destroy', ['id' => $p->id])}}" method="post" enctype="multipart/form-data">
-                        @method('delete')
-                        @csrf
-                        <div class="modal fade" id="staticBackdropE{{$p->id}}" data-bs-backdrop="static" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title  font-weight-bolder" id="staticBackdropLabel">Eliminar producto</h5>
-                                    </div>
-                                    <div class="modal-body">
-                                        ¿Esta seguro de borrar el producto: {{$p->nombre}}?
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="submit" class="btn btn-danger">Si</button>
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+            <abbr title="hola">
+                <tr style="text-align:center;">
+                    <td scope="col">{{++$i}}</td>
+                    <td scope="col" style="text-align:center; display: nome">{{$p->id}}</td>
+                    <td scope="col" style="text-align:center; display: nome">{{$p->peso}}</td>
+                    <td scope="col">{{$p->nombre}}</td>
+                    <td scope="col">{{$p->tipo_producto->descripcion}}</td>
+                    <td scope="col">
+                        <!--Boton para restar cantidad-->
+                        <a style="color: red;" class="productpiscina" data-toggle="modal" data-target="#restar{{$p->id}}">
+                            <i class="fa fa-minus-circle" aria-hidden="true"></i>
+                        </a>
+                        <!--Texto de cantidad-->
+                        {{$p->peso}} @if ($p->tipo_producto->id == 1) Libras @else Onzas @endif
+                        <!--Boton para agregar cantidad-->
+                        <a style="color: blue;" class="productpiscina" data-toggle="modal" data-target="#agregar{{$p->id}}">
+                            <i style="text-size-adjust: 2px;" class="fa fa-plus-circle" aria-hidden="true"></i>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="{{ route('producto.edit', ['id' => $p->id]) }}">
+                            <i class="fa-solid fa-edit text-success" style="color:rgb(33, 195, 247)"></i>
+                    </td>
+                    <!----icono para borrar el producto------>
+                    <td>
+                        <i data-bs-toggle="modal" data-bs-target="#staticBackdropE{{$p->id}}" class="fa-solid fa-trash-can text-danger" style="color:crimson"></i>
+                        <form action="{{route('prodpiscina.destroy', ['id' => $p->id])}}" method="post" enctype="multipart/form-data">
+                            @method('delete')
+                            @csrf
+                            <div class="modal fade" id="staticBackdropE{{$p->id}}" data-bs-backdrop="static" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title  font-weight-bolder" id="staticBackdropLabel">Eliminar producto</h5>
+                                        </div>
+                                        <div class="modal-body">
+                                            ¿Esta seguro de borrar el producto: {{$p->nombre}}?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="submit" class="btn btn-danger">Si</button>
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </form>
-                </td>
-            </tr>
+                        </form>
+                    </td>
+                </tr>
+            </abbr>
+
             <!--Modal de agregar cantidad-->
             <div class="modal fade" id="agregar{{$p->id}}" tabindex="-1" role="dialog" aria-hidden="false" data-backdrop="static">
                 <div class="modal-dialog modal-dialog-centered" role="document">
